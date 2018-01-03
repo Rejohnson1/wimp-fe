25bbaa51b0ee1e4aa5e836b05bc51358e1155330
Rob Johnson <rob.johnson@libertymutual.com>
2018-01-03 13:33:27 -0500

    front end wimp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { wimpService } from './wimp.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  movies;
+  actors;
+ 
+  constructor(private wimpservice: wimpService){}
+
+  getMovies(){
+
+    this.wimpservice.getRecords("movies")
+      .subscribe(moviesFromAPI => {
+        this.movies = moviesFromAPI;
+      })
+  }
+
+onSubmitMovie(movieData: NgForm) {
+  this.wimpservice.addRecord("movies", movieData.value)
+  .subscribe(addMovie => {
+    this.getMovies();
+    movieData.reset()
+  })
+}
+ 
+onSubmitActor(actorData: NgForm) {
+  this.wimpservice.addRecord("actors", actorData.value)
+  .subscribe(addActor => {
+    this.getActors();
+    actorData.reset()
+  })
+}
+
+
+  getActors(){
+          this.wimpservice.getRecords("actors")
+          .subscribe(actorsFromAPI => {
+            this.actors = actorsFromAPI;
+          })
+    
+      }
+
+  ngOnInit(){
+    this.getMovies();
+    this.getActors();
+  }
+
 }
